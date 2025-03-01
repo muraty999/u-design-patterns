2688063b6538c8a3943a5a09c52ac83aec58b54f
Murat Yilmaz <muraty999@gmail.com>
2025-03-01 09:04:34 +0300

    part4 delegation principles - correct class name

--- Part4PrinciplesAndStrategiesOfDesign/src/academy/learnprogramming/DelegationTester.java
+++ Part4PrinciplesAndStrategiesOfDesign/src/academy/learnprogramming/DelegationTester.java
@@ -17,7 +17,7 @@ class Printer {
     }
 }
 
-class Tester {
+public class DelegationTester {
     // To the outside world it looks like Printer actually prints
     public static void main(String[] args) {
         Printer printer = new Printer();
