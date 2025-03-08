5d0979617a038f9dd4f999230c64ec86882f2ed1
Murat Yilmaz <muraty999@gmail.com>
2025-03-08 06:56:51 +0300

    Single responsibility files created

---
+++ Part4PrinciplesAndStrategiesOfDesign/src/academy/learnprogramming/singleresponsibility/Finance.java
@@ -0,0 +1,8 @@
+package academy.learnprogramming.singleresponsibility;
+
+public class Finance {
+    public double calcIncomeTaxForCurrentYear(Employee emp) {
+        // income tax logic using employee passed in
+        return 0.0;
+    }
+}
