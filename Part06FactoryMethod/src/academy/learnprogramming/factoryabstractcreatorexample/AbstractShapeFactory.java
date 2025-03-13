e75ab4c62116d443eda5636120e4eedecbb8676c
Murat Yilmaz <muraty999@gmail.com>
2025-03-13 23:20:46 +0300

    Factory Abstract Creator example files

---
+++ Part06FactoryMethod/src/academy/learnprogramming/factoryabstractcreatorexample/AbstractShapeFactory.java
@@ -0,0 +1,31 @@
+package academy.learnprogramming.factoryabstractcreatorexample;
+
+public abstract class AbstractShapeFactory {
+    protected abstract Shape factoryMethod();
+    public Shape getShape() {
+        return factoryMethod();
+    }
+    // other helper methods
+}
+
+class RectangleFactory extends AbstractShapeFactory {
+    protected Shape factoryMethod()
+    {
+        return new Rectangle();
+    }
+}
+
+class SquareFactory extends AbstractShapeFactory {
+    protected Shape factoryMethod()
+    {
+        return new Square();
+    }
+}
+
+class CircleFactory extends AbstractShapeFactory {
+    protected Shape factoryMethod()
+    {
+        return new Circle();
+    }
+}
+
